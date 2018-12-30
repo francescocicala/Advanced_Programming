4d4c52bcaeb51bc64334ebeceeb5510186a4b6aa
francescocicala <francesco.cicala00@gmail.com>
2018-12-30 15:45:55 +0100

    minor changes

--- lectures/c++/03_my_pointers_and_vectors/01_auto.cc
+++ lectures/c++/03_my_pointers_and_vectors/01_auto.cc
@@ -1,41 +1,39 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
 #if __cplusplus > 201103L
 
 template <class T>
-auto init(const std::size_t l) {
+auto init(const size_t l) {
   return new T[l]{};
 }
 
 #else
 
 template <class T>
-T* init(const std::size_t l) {
+T* init(const size_t l) {
   return new T[l]{};
 }
 
 #endif
 
 int main() {
-  auto b = true;
-  auto ch = 'x';
-  auto i = 123;
-  auto d = 1.2;
-  auto z = std::sqrt(d);
-  auto ui = 0u;
-  auto llui = 0llu;
-
-  std::cout << "__cplusplus = " << __cplusplus << std::endl;
+  auto b = true;          // a bool
+  auto ch = 'x';          // a char
+  auto i = 123;           // an int
+  auto d = 1.2;           // a double
+  auto z = std::sqrt(d);  // z has the type of whatever sqrt(d) returns
+  auto ui = 0u;           // unsigned int
+  auto llui = 0llu;       // long long unsigned int
 
   for (auto i = 0; i < 7; ++i)
-    std::cout << i << std::endl;
+    cout << i << endl;
 
   auto pb = &b;
-  auto ps = init<double>(11);
-  for (std::size_t i{0}; i < 11; ++i)
-    std::cout << "ps[" << i << "] = " << ps[i] << std::endl;
 
+  auto ps = init<double>(11);
   delete[] ps;
   return 0;
 }
--- lectures/c++/03_my_pointers_and_vectors/02_const_and_pointers.cc
+++ lectures/c++/03_my_pointers_and_vectors/02_const_and_pointers.cc
@@ -1,39 +1,36 @@
 #include <iostream>
 
+using namespace std;
+
 template <typename T>
-void print_sentinel(const T* a, const std::size_t l);
+void print_sentinel(const T* a, const size_t l);
 
 int main() {
   int a{8}, b{7};
   int* pi{&a};
 
-  const int* pc = &a; // pointer where the pointed value cannot be changed
-                      // by mean of the pointer
-  // *pc = 7; // error
+  const int* pc = &a; // pointer to a constant integer
+  // *pc = 7; //error
   a = 15;
 
-  int* const cp = &a; // the memory location cannot be changed
+  int* const cp = &a; // constant pointer
   a = b;
   *cp = 33;
   // cp = &b; // error
 
-  const int* const cpc = &a; // points to a fixed memory location
-                             // and cannot change it
-  // *cpc = 0;  // error
-  // cpc = &n;  // error
+  const int* const cpc = &a; // constant pointer to constant integer
+  a = 99;
 
-  a = 99; 
-
-
-  int aa[4]{1, 2, 3, 4};
+  int aa[4]{};
   print_sentinel(aa, 4);
+
+  return 0;
 }
 
+
 template <typename T>
-void print_sentinel(const T* a, const std::size_t l) {
-  const T* const end{a + l}; // end is a pointer containing the memory location following
-                             // the one in which the last element of the array is stored
-  for (; a!= end; ++a)
-    std::cout << *a << std::endl;
-  std::cout << "*(end - 1) = " << *(end - 1) << "\nend = " << end << std::endl;
+void print_sentinel(const T* a, const size_t l) {
+  const T* const end{a + l};
+  for (; a != end; ++a)
+    cout << *a << " in " << a << endl;
 }
